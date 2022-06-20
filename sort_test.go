1ddc4ff3f0f2016787488fbd4b148aa696423c97
Jonas Cavalcanti <jonas.cavalcanti@unico.io>
2022-06-20 11:12:58 -0300

    Update

--- sort_test.go
+++ sort_test.go
@@ -1,11 +1,10 @@
 package main
 
 import "testing"
-import "go-labs/sort"
 
 func SelectionSortTest(t *testing.T) {
 	numb := [5]int{1, 2, 3, 4, 5}
-	result := sort.SelectionSort(numb)
+	result := SelectionSort(numb)
 
 	if !result {
 		t.Errorf("result %d", result)
