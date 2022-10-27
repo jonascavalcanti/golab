9cd73175887d801487a7c5899f44094fea3c690d
Jonas Cavalcanti <jonas.cavalcanti@unico.io>
2022-10-27 17:58:30 -0300

    Update

--- gopl.io/ch1/echo.go
+++ gopl.io/ch1/echo.go
@@ -1,13 +1,33 @@
 package ch1
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func Echo(args []string) {
 	var s string
 
-	for i := 1; i < len(args); i++ {
-		s += args[i] + " "
+	//First format
+	for _, args := range os.Args[1:] {
+		s += args + " "
+	}
+	fmt.Println(s)
+
+	//Second format
+	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	fmt.Println("Exercise 1.1:")
+	if len(os.Args) < 2 {
+		fmt.Printf("Example: %s <string 1> <string 2> ...", os.Args[0])
+		return
 	}
 
-	fmt.Println(s)
+	fmt.Println("Exercise 1.2:")
+	for idx, args := range os.Args[1:] {
+		fmt.Println(idx, ":", args)
+
+	}
+
 }
