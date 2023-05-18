91dd4fe1a2cf281d48b34b21b49c31681223a921
Jonas Cavalcanti <jonas.cavalcanti@unico.io>
2023-05-18 10:42:17 -0300

    Update

--- gopl/ch1/main.go
+++ gopl/ch1/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/jonascavalcantineto/golabs/gopl/ch1/handler"
 )
 
 func main() {
@@ -12,16 +14,16 @@ func main() {
 	fmt.Println()
 
 	fmt.Println("Echo version 1 is using normal for() statement:")
-	lib.Echo1(os.Args)
+	handler.Echo1(os.Args)
 
 	fmt.Println()
 
 	fmt.Println("Echo version 2 is using range for() statement:")
-	lib.Echo2(os.Args)
+	handler.Echo2(os.Args)
 
 	fmt.Println()
 
 	fmt.Println("Echo version 3 is using package strings.Join() statement:")
-	lib.Echo3(os.Args)
+	handlers.Echo3(os.Args)
 
 }
