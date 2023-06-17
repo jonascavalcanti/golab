e67a61b5ef84ba21c5a92482dbec1dd65e0a6264
Jonas Cavalcanti <jonas.cavalcanti@unico.io>
2023-06-17 15:01:38 -0300

    Organizing paths

---
+++ books/gopl/ch1/echo.go
@@ -0,0 +1,47 @@
+package ch1
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func Echo1(args []string) {
+	start := time.Now()
+
+	var s, sep string
+
+	for i := 1; i < len(args); i++ {
+		s += sep + args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+
+}
+
+func Echo2(args []string) {
+	start := time.Now()
+	var s, sep string
+
+	for idx, arg := range args[1:] {
+		s += sep + arg
+		sep = " "
+
+		fmt.Println("idx: ", idx, "arg: ", arg)
+	}
+
+	fmt.Println(s)
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func Echo3(args []string) {
+	start := time.Now()
+
+	fmt.Println(strings.Join(args[1:], " "))
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+
+}
---
+++ books/gopl/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jonascavalcantineto/golab/gopl/ch1"
+)
+
+func main() {
+
+	fmt.Println("Program Name:", os.Args[0])
+
+	fmt.Println()
+
+	fmt.Println("Echo version 1 is using normal for() statement:")
+	ch1.Echo1(os.Args)
+
+	fmt.Println()
+
+	fmt.Println("Echo version 2 is using range for() statement:")
+	ch1.Echo2(os.Args)
+
+	fmt.Println()
+
+	fmt.Println("Echo version 3 is using package strings.Join() statement:")
+	ch1.Echo3(os.Args)
+
+}
